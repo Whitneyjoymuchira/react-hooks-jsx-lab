678b1708a73267d8a0b9af5ce523d43b5becdafc
Whitney Muchira <whitneymuchira@gmail.com>
2022-07-13 01:51:09 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { image } from "../data/data";
+import {image} from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I am a self-taught baker specializing in wedding cakes </p>
+    <img src={image} alt= "I made this" />
+
+  </div>
+
+  ) 
 }
 
 export default About;
